64fdf07dda6e51003b90656a62604479324ad4e7
Dany Dumais <dumais_dan@hotmail.fr>
2019-02-15 17:29:56 -0500

    New feature :  wifi connectivity & blynk librarie

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,19 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "YourNetworkName";
+char pass[] = "YourPassword";
 
 #define LED1 9
 #define LED2 10
