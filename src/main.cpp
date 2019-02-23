a6c72404fab3ae7854b02c26ba1eca391e955fc6
Dany Dumais <dumais_dan@hotmail.fr>
2019-02-23 10:27:05 -0500

    Adding three task (Working)

--- src/main.cpp
+++ src/main.cpp
@@ -10,27 +10,18 @@
 ⋆ 
 ⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆*/
 
-#include <Arduino.h>
-#include <esp_wifi.h>
+
+
 #include <esp_bt.h>
-#include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 #include <Stepper.h>
 #include "soc/rtc.h"
 #include "esp32/pm.h"
-#include "Esp.h"
-#include "esp_task.h"
-#include "esp_err.h"
 #include "esp_bt_main.h"
 
 
 /* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
-
-//task handle
-TaskHandle_t TaskCom;
-TaskHandle_t TaskMot;
-TaskHandle_t TaskSen;
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
@@ -50,7 +41,12 @@ WidgetLED led(V2);
 //sub program declaration
 void stepperMotor();
 void blink();
-void consoleDebug(String String1ToPrint,String String2ToPrint);
+void consoleDebug(String String1ToPrint);
+
+//task handle
+TaskHandle_t TaskCom;
+TaskHandle_t TaskMot;
+TaskHandle_t TaskSen;
 
 //task declaration
 void TaskMoteur(void * parameter);
@@ -67,22 +63,24 @@ Stepper myStepper(stepsPerRevolution, 23, 19, 22, 21);
 //variable
 int ledState = LOW;   
 int stepCount = 0;  // number of steps the motor has taken
-unsigned long previousMillis = 0;  
+
  
-void setup() 
+void setup()
 {
-  //close what we don't use
+
+    //close what we don't use
   esp_bluedroid_disable();
   esp_bt_controller_disable();
 
   //enable wakup sourcce
-  esp_sleep_enable_timer_wakeup(10000000);
+  //esp_sleep_enable_timer_wakeup(10000000);
    
   // Set pins modes
   pinMode(25,INPUT);
 
   //Connect to wifi
   Blynk.begin(auth, ssid, pass);
+  delay(500); 
 
   // This will print Blynk Software version to the Terminal Widget when
   // your hardware gets connected to Blynk Server
@@ -92,41 +90,71 @@ void setup()
   terminal.println(F("anything else and get it printed back."));
   terminal.flush();
 
-
+  delay(500); 
 
   //initialsation of new task 
-  //xTaskCreatePinnedToCore(TaskCommunication, "TaskCom", 1000, NULL, 1, &TaskCom, 1);
-  //xTaskCreatePinnedToCore(TaskMoteur, "TaskMot", 1000, NULL, 1, &TaskMot, 1);
-  //xTaskCreatePinnedToCore(TaskSensor, "TaskSen", 1000, NULL, 1, &TaskSen, 1);
+  xTaskCreatePinnedToCore(
+                    TaskCommunication,          /* Task function. */
+                    "TaskCom",                  /* String with name of task. */
+                    10000,                      /* Stack size in bytes. */
+                    NULL,                       /* Parameter passed as input of the task */
+                    15,                         /* Priority of the task. */
+                    NULL,                       /* task handler. */     
+                    1);                         /* Core id. */
+
+  delay(500); 
+
+    xTaskCreatePinnedToCore(
+                    TaskMoteur,                 /* Task function. */
+                    "TaskMot",                  /* String with name of task. */
+                    10000,                      /* Stack size in bytes. */
+                    NULL,                       /* Parameter passed as input of the task */
+                    15,                         /* Priority of the task. */
+                    NULL,                       /* task handler. */     
+                    1);                         /* Core id. */
+
+  delay(500); 
+
+      xTaskCreatePinnedToCore(
+                    TaskSensor,                  /* Task function. */
+                    "TaskSen",                  /* String with name of task. */
+                    10000,                      /* Stack size in bytes. */
+                    NULL,                       /* Parameter passed as input of the task */
+                    15,                         /* Priority of the task. */
+                    NULL,                       /* task handler. */     
+                    1);                         /* Core id. */
+
+  delay(500); 
 
   //changing cpu speed
   rtc_clk_cpu_freq_set(RTC_CPU_FREQ_80M);
 
   //initial setup of the blind
-  
+
 }
  
-void loop() 
-{
-  
-  consoleDebug("test7","test8");
-  //get core id running on
-  //int test = xPortGetCoreID(); 
-
-
-}
-
 void TaskCommunication(void * parameter)
 {
   //setup for comz 
-
-  //run task comz
-  for(;;)
+  unsigned long currentMillis;
+  unsigned long nextMillis = 0;  
+  /* The parameter value is expected to be 1 as 1 is passed in the
+  pvParameters value in the call to xTaskCreate() below.*/ 
+  //configASSERT( ( ( uint32_t ) parameter ) == 1 );
+ 
+ //run task comz
+  while(1)
   {
-    //give ping to blynk and get value if change
-    Blynk.run();
+    currentMillis = millis();
+    if (currentMillis  > nextMillis + 7000)
+    {
+      consoleDebug("test0");
+      nextMillis = currentMillis;
+    }
 
-    consoleDebug("test2","test");
+
+    //give ping to blynk and get value if change
+    //Blynk.run();
 
     //if no new value shut down wifi
     //esp_wifi_stop();
@@ -135,18 +163,36 @@ void TaskCommunication(void * parameter)
     //esp_light_sleep_start() ;
     
   }
+  vTaskDelete( NULL );
+
 }
 
 void TaskMoteur(void * parameter)
 {
   //setup for motor
   myStepper.setSpeed(8); // 8 rpm
+  unsigned long currentMillis;
+  unsigned long nextMillis = 0;
+
+  /* The parameter value is expected to be 1 as 1 is passed in the
+  pvParameters value in the call to xTaskCreate() below.*/ 
+ 
+
   //run task motor
-  for(;;)
+  while(1)
   {
-    //stepperMotor();
+    //step.perMotor();
+
+    currentMillis = millis();
+    if (currentMillis  > nextMillis + 7000)
+    {
+      consoleDebug("test1");
+      nextMillis = currentMillis;
+    }
+
+
     //si manuelle attend un mouvement des GPIO/Blynk
-      consoleDebug("test3","test4");
+
       //compte le nombre de step a tournee 
 
       //met a jour blynk
@@ -161,16 +207,25 @@ void TaskMoteur(void * parameter)
 
       //retourne a attendre une nouvelle commande
   }
+  vTaskDelete( NULL );
 }
 
 void TaskSensor(void * parameter)
 {
   //setup for sensor
-
+  unsigned long currentMillis ;
+  unsigned long nextMillis = 0;  
   //run task sensor
-  for(;;)
+  while(1)
   {
-    consoleDebug("test5","test6");
+    currentMillis = millis();
+    if (currentMillis  > nextMillis + 7000)
+    {
+      consoleDebug("test2");
+      nextMillis = currentMillis;
+    }
+
+   
     //A chaque X wake up 
 
     //update inside temp
@@ -179,12 +234,14 @@ void TaskSensor(void * parameter)
 
     //update sun on the window
   }
+  vTaskDelete( NULL );
 }
 
 void blink()
 {
   //use de millis function because non blocking function and when cpu freq is change delay() suck
   unsigned long currentMillis = millis();
+  unsigned long previousMillis = 0;  
 
   if (currentMillis - previousMillis >= interval) {
     // save the last time you blinked the LED
@@ -224,11 +281,17 @@ void stepperMotor()
 
 }
 
-void consoleDebug(String String1ToPrint,String String2ToPrint)
+void consoleDebug(String String1ToPrint)
 {
-  terminal.print(String1ToPrint);
-  terminal.print(String2ToPrint);
-  terminal.flush();
   Blynk.run();
+  terminal.print(String1ToPrint);
+  terminal.flush();
+  
   return;
+}
+
+void loop() 
+{
+  
+
 }
