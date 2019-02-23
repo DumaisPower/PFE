fe7f192039475d83fa349eb51c1073f1d594a2e5
Dany Dumais <dumais_dan@hotmail.fr>
2019-02-23 18:59:40 -0500

    Adding sleep/wake feature

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 ⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆⋆*/
 
 
+#include "BluetoothSerial.h" 
 
 #include <esp_bt.h>
 #include <BlynkSimpleEsp32.h>
@@ -18,6 +19,11 @@
 #include "soc/rtc.h"
 #include "esp32/pm.h"
 #include "esp_bt_main.h"
+#include "esp_wifi.h"
+#include "esp_sleep.h"
+#include "freertos/FreeRTOS.h"
+#include "esp_system.h"
+#include "esp_event.h"
 
 
 /* Comment this out to disable prints and save space */
@@ -41,12 +47,15 @@ WidgetLED led(V2);
 //sub program declaration
 void stepperMotor();
 void blink();
-void consoleDebug(String String1ToPrint);
+void consoleDebug(String StringToPrint);
+void consoleErr(esp_err_t err,String StringToPrint);
+void GoToSleep();
 
 //task handle
 TaskHandle_t TaskCom;
 TaskHandle_t TaskMot;
 TaskHandle_t TaskSen;
+int nTask = 3;
 
 //task declaration
 void TaskMoteur(void * parameter);
@@ -64,33 +73,58 @@ Stepper myStepper(stepsPerRevolution, 23, 19, 22, 21);
 int ledState = LOW;   
 int stepCount = 0;  // number of steps the motor has taken
 
- 
+wifi_init_config_t config ;
+BluetoothSerial ESP_BT;
+
+//define semaphore for task manager
+SemaphoreHandle_t SemaphoreMotor = xSemaphoreCreateCounting( 1, 0 );
+SemaphoreHandle_t SemaphoreSensor = xSemaphoreCreateCounting( 1, 0 );
+SemaphoreHandle_t SemaphoreBarrier = xSemaphoreCreateCounting( nTask, 0 );
+
 void setup()
 {
+  //set initial serial communication
+  ESP_BT.begin("ESP32_BT");
+  Serial.begin(9600);
 
-    //close what we don't use
-  esp_bluedroid_disable();
-  esp_bt_controller_disable();
-
-  //enable wakup sourcce
-  //esp_sleep_enable_timer_wakeup(10000000);
-   
   // Set pins modes
   pinMode(25,INPUT);
 
   //Connect to wifi
   Blynk.begin(auth, ssid, pass);
-  delay(500); 
+
+  delay(300); 
+
+  //clear terminal
+  terminal.clear();
 
   // This will print Blynk Software version to the Terminal Widget when
   // your hardware gets connected to Blynk Server
   terminal.println(F("Blynk v" BLYNK_VERSION ": Device started"));
   terminal.println(F("-------------"));
-  terminal.println(F("Type 'Marco' and get a reply, or type"));
-  terminal.println(F("anything else and get it printed back."));
-  terminal.flush();
+  
+  /* If we use other app than blynk it might be useful*/
+  // consoleErr(esp_wifi_init(&config), "esp_wifi_init : ");
+  // delay(300); 
+  // consoleErr(esp_wifi_start(), "esp_wifi_start : ");
+  // delay(300); 
+  // consoleErr(esp_wifi_set_auto_connect(1), "esp_wifi_set_auto_connect : ");
+  
 
-  delay(500); 
+  //enable wakup sourcce
+  consoleErr(esp_sleep_enable_timer_wakeup(8000000), "esp_sleep_enable_timer_wakeup : ");
+  consoleErr(esp_sleep_enable_ulp_wakeup(), "esp_sleep_enable_ulp_wakeup : ");
+  //consoleErr(gpio_wakeup_enable(), "gpio_wakeup_enable : ");
+  //consoleErr(esp_sleep_enable_gpio_wakeup(), "esp_sleep_enable_gpio_wakeup : ");
+
+  //close what we don't use
+  delay(300); 
+  consoleErr(esp_bluedroid_disable(), "esp_bluedroid_disable : ");
+  delay(300); 
+  consoleErr(esp_bt_controller_disable(), "esp_bt_controller_disable : ");
+  delay(300); 
+  terminal.flush();
+  delay(300); 
 
   //initialsation of new task 
   xTaskCreatePinnedToCore(
@@ -127,7 +161,7 @@ void setup()
   delay(500); 
 
   //changing cpu speed
-  rtc_clk_cpu_freq_set(RTC_CPU_FREQ_80M);
+  //rtc_clk_cpu_freq_set(RTC_CPU_FREQ_80M);
 
   //initial setup of the blind
 
@@ -138,6 +172,8 @@ void TaskCommunication(void * parameter)
   //setup for comz 
   unsigned long currentMillis;
   unsigned long nextMillis = 0;  
+  String err;
+
   /* The parameter value is expected to be 1 as 1 is passed in the
   pvParameters value in the call to xTaskCreate() below.*/ 
   //configASSERT( ( ( uint32_t ) parameter ) == 1 );
@@ -152,19 +188,34 @@ void TaskCommunication(void * parameter)
       nextMillis = currentMillis;
     }
 
-
     //give ping to blynk and get value if change
-    //Blynk.run();
 
-    //if no new value shut down wifi
-    //esp_wifi_stop();
+    // //if no new value shut down wifi
+    GoToSleep();
+  
 
-    //go to sleep for 10 sec or GPIO press
-    //esp_light_sleep_start() ;
-    
   }
   vTaskDelete( NULL );
 
+}
+
+void GoToSleep()
+{
+  consoleDebug("Entering sleep mode");
+
+  //stop wifi connection
+  esp_wifi_stop();
+  
+  //go to sleep for 7 sec or GPIO press
+  esp_light_sleep_start();
+ 
+  //restart wifi
+  esp_wifi_start();
+
+  //connect to blynk
+  Blynk.begin(auth, ssid, pass);
+
+  consoleDebug("just woke up");
 }
 
 void TaskMoteur(void * parameter)
@@ -176,7 +227,7 @@ void TaskMoteur(void * parameter)
 
   /* The parameter value is expected to be 1 as 1 is passed in the
   pvParameters value in the call to xTaskCreate() below.*/ 
- 
+ //configASSERT( ( ( uint32_t ) parameter ) == 1 );
 
   //run task motor
   while(1)
@@ -187,6 +238,8 @@ void TaskMoteur(void * parameter)
     if (currentMillis  > nextMillis + 7000)
     {
       consoleDebug("test1");
+      terminal.println(millis());
+      Blynk.run();
       nextMillis = currentMillis;
     }
 
@@ -216,6 +269,11 @@ void TaskSensor(void * parameter)
   unsigned long currentMillis ;
   unsigned long nextMillis = 0;  
   //run task sensor
+
+  /* The parameter value is expected to be 1 as 1 is passed in the
+  pvParameters value in the call to xTaskCreate() below.*/ 
+ //configASSERT( ( ( uint32_t ) parameter ) == 1 );
+
   while(1)
   {
     currentMillis = millis();
@@ -267,11 +325,6 @@ void stepperMotor()
 {
   // read the sensor value:
 
-
-  // map it to a range from 0 to 100:
-  //int motorSpeed = map(500, 0, 1023, 0, 100);
-  
-  
   myStepper.step(2038); // do 2038 steps -- corresponds to one revolution in one minute
    // wait for one second
   // do 2038 steps in the other direction with faster 
@@ -281,10 +334,21 @@ void stepperMotor()
 
 }
 
-void consoleDebug(String String1ToPrint)
+void consoleErr(esp_err_t  err, String StringToPrint)
 {
+  const char * err_name;
+  err_name = esp_err_to_name(err);
+  terminal.println(StringToPrint + err_name );
+  terminal.flush();
+  
+  return;
+}
+
+void consoleDebug(String StringToPrint)
+{
+  terminal.println(StringToPrint);
   Blynk.run();
-  terminal.print(String1ToPrint);
+  delay(200);
   terminal.flush();
   
   return;
